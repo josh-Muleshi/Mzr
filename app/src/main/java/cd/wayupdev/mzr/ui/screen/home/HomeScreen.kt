9774b5f389aa882ca311f15afe1e12defb3b7125
josh-Muleshi <jmuleshi2@gmail.com>
2022-03-29 15:07:41 +0200

    add the top app bar et ther drawer menu

--- app/src/main/java/cd/wayupdev/mzr/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/cd/wayupdev/mzr/ui/screen/home/HomeScreen.kt
@@ -3,11 +3,15 @@ package cd.wayupdev.mzr.ui.screen.home
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
@@ -17,5 +21,37 @@ fun HomeScreen(navController: NavHostController) {
     BackHandler(enabled = true) {
         (context as? Activity)?.finish()
     }
-    Text(text = "Salut le monde", color = Color.Black, modifier = Modifier.padding(16.dp))
+    Scaffold(
+        topBar = {
+            TopPageBar(navController)
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}, backgroundColor = MaterialTheme.colors.primary){
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ){
+        Text(text = "Salut le monde", color = Color.Black, modifier = Modifier.padding(16.dp))
+    }
+}
+
+@Composable
+fun TopPageBar(navController: NavHostController) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "AuthBoy Mzr",
+                color = Color.White
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(imageVector = Icons.Default.Menu, "menu")
+            }
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+        elevation = 12.dp
+    )
 }
