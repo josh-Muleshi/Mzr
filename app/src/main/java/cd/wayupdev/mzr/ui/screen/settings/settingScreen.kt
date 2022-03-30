0fda33b6659a2777897acec7cbb066fc2481ad0a
josh-Muleshi <jmuleshi2@gmail.com>
2022-03-30 03:56:42 +0200

    setting screen done

--- app/src/main/java/cd/wayupdev/mzr/ui/screen/settings/settingScreen.kt
+++ app/src/main/java/cd/wayupdev/mzr/ui/screen/settings/settingScreen.kt
@@ -1,21 +1,40 @@
 package cd.wayupdev.mzr.ui.screen.settings
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import cd.wayupdev.mzr.R
 import cd.wayupdev.mzr.ui.screen.topAppBar.AppBarScreen
 
 @Composable
 fun SettingScreen(navController: NavHostController) {
+    val names : List<String> = listOf("Language", "Dark Mode", "Help")
     Scaffold(
         topBar = {
             AppBarScreen(navController, ScreenName = "Settings")
         }
     ){
-        Text(text = "Settings", modifier = Modifier.padding(16.dp), color = Color.Black)
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            names.forEach { name ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = name, modifier = Modifier.padding(16.dp), color = Color.Black)
+                    Icon(painter = painterResource(id = R.drawable.ic_arrow_righ), contentDescription = null)
+                }
+                Divider()
+            }
+
+        }
+
     }
 }
