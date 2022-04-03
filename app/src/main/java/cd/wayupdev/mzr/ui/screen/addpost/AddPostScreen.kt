37d9b4c04c19e8dc33e40ba9d8ef0a10393bc5cd
josh-Muleshi <jmuleshi2@gmail.com>
2022-04-03 23:59:28 +0200

    finish

--- app/src/main/java/cd/wayupdev/mzr/ui/screen/addpost/AddPostScreen.kt
+++ app/src/main/java/cd/wayupdev/mzr/ui/screen/addpost/AddPostScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -61,7 +62,7 @@ fun AddPostScreen(navController : NavHostController) {
 fun CustomTextField() {
     TextField(
         modifier = Modifier.fillMaxWidth(),
-        value = "null",
+        value = "",
         onValueChange = {
             //onTextChange(it)
         },
