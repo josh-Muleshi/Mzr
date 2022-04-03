6d03a5fe43ee1fee8d06a84b67c2a9533a1d8613
josh-Muleshi <jmuleshi2@gmail.com>
2022-04-03 22:11:52 +0200

    well fix my ui

--- app/src/main/java/cd/wayupdev/mzr/ui/screen/addpost/AddPostScreen.kt
+++ app/src/main/java/cd/wayupdev/mzr/ui/screen/addpost/AddPostScreen.kt
@@ -8,19 +8,20 @@ import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import cd.wayupdev.mzr.R
 import cd.wayupdev.mzr.ui.screen.topAppBar.AppBarScreen
 
 @Composable
@@ -48,15 +49,7 @@ fun RequestContentPermission() {
     ActivityResultContracts.GetContent()) { uri: Uri? ->
         imageUri = uri
     }
-    Column() {
-        Button(onClick = {
-            launcher.launch("image/*")
-        }) {
-            Text(text = "Pick image")
-        }
-
-        Spacer(modifier = Modifier.height(12.dp))
-
+    Column(modifier = Modifier.padding(16.dp)) {
         imageUri?.let {
             if (Build.VERSION.SDK_INT < 28) {
                 bitmap.value = MediaStore.Images
@@ -73,6 +66,14 @@ fun RequestContentPermission() {
                     contentDescription =null,
                     modifier = Modifier.size(400.dp))
             }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            launcher.launch("image/*")
+        }) {
+            Icon(painter = painterResource(id = R.drawable.ic_photo_camera), contentDescription = null, modifier = Modifier.padding(25.dp))
         }
 
     }
