763cc5b989f4a0cca397780d33aaa2d7a2a1d4ad
josh-Muleshi <jmuleshi2@gmail.com>
2022-04-16 14:03:22 +0200

    add an exemple file

---
+++ app/src/main/java/cd/wayupdev/mzr/data/ShowList.kt
@@ -0,0 +1,24 @@
+package cd.wayupdev.mzr.data
+
+import cd.wayupdev.mzr.R
+import cd.wayupdev.mzr.data.model.Post
+
+object ShowList {
+
+    val itShows = listOf(
+        Post(
+            uid = "0",
+            title = "Concert",
+            adminUid = "0",
+            description = "bal bla bla bla dhjii iujhijklc uih ijnkdshu nbuihuhiuji uhiuhsdjidsu u7hidsjj yuh8jij ugy7juhuj",
+            imageUrl = R.drawable.ic_android.toString()
+        ),
+        Post(
+            uid = "0",
+            title = "Concert",
+            adminUid = "0",
+            description = "bal bla bla bla dhjii iujhijklc uih ijnkdshu nbuihuhiuji uhiuhsdjidsu u7hidsjj yuh8jij ugy7juhuj",
+            imageUrl = R.drawable.ic_android.toString()
+        )
+    )
+}
